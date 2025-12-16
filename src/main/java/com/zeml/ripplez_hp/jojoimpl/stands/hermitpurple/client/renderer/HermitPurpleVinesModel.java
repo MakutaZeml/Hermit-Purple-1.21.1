fd173df1161a9cc8f8a709536d0c3e375de4ad2a
Makuta Zeml <zzahir9@gmail.com>
2025-12-15 21:52:57 -0600

    new animations and model
    finally, new animations

--- src/main/java/com/zeml/ripplez_hp/jojoimpl/stands/hermitpurple/client/renderer/HermitPurpleVinesModel.java
+++ src/main/java/com/zeml/ripplez_hp/jojoimpl/stands/hermitpurple/client/renderer/HermitPurpleVinesModel.java
@@ -10,13 +10,20 @@ import net.minecraft.client.model.geom.ModelPart;
 public class HermitPurpleVinesModel extends HumanoidModel {
     public final ModelPart vines;
     public final ModelPart grab_vine;
+    public final ModelPart main_vine;
+    public final ModelPart main_vine2;
+    public final ModelPart main_vine3;
     
     public HermitPurpleVinesModel(ModelPart root) {
         super(HumanoidPlayerModel.addMissingBoneDefinitions(root));
         
-        this.vines = ((Model_1_21_2plus) this).jojo_ripples$getAnyDescendantWithName("hidden#vines").orElse(null);
-        this.grab_vine = ((Model_1_21_2plus) this).jojo_ripples$getAnyDescendantWithName("hidden#vines").orElse(null);
-        
+        this.vines = ((Model_1_21_2plus) this).jojo_ripples$getAnyDescendantWithName("vines").orElse(null);
+        this.grab_vine = ((Model_1_21_2plus) this).jojo_ripples$getAnyDescendantWithName("hidden#grab_vine").orElse(null);
+        this.main_vine = ((Model_1_21_2plus) this).jojo_ripples$getAnyDescendantWithName("hidden#main_vine").orElse(null);
+        this.main_vine2 = ((Model_1_21_2plus) this).jojo_ripples$getAnyDescendantWithName("hidden#main_vine2").orElse(null);
+        this.main_vine3 = ((Model_1_21_2plus) this).jojo_ripples$getAnyDescendantWithName("hidden#main_vine3").orElse(null);
+
+
         HiddenModelPartsUtil.initHiddenParts(this);
     }
 }
